98a0c6292eebcba960d3243e7761a7f0ad0936f8
drexly <drexly.com@gmail.com>
2017-10-27 08:16:15 +0900

    Revert "Experimented Data"
    This reverts commit 71f5a27b682e8d5317b66a46e5c74b0be1a4689b.

--- teamTAKEDOWN/Experiments/test1.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.0.7', 6000))
-
-while True:
-    data = '0|1000|GeneralResult|MachineLearntResult|ClientStatus'
-    sock.send(data)
-    print "response: ", sock.recv(1024)
--- teamTAKEDOWN/Experiments/test7.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.0.7', 6240))
-
-while True:
-    data = '240|90|GeneralResult|MachineLearntResult|ClientStatus'
-    sock.send(data)
-    print "response: ", sock.recv(1024)
